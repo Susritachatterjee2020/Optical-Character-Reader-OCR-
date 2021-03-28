a9ff22b4066bae21e96d7c51ec59db182f05ebb0
Susrita Chatterjee <60474018+Susritachatterjee2020@users.noreply.github.com>
2021-03-28 20:47:54 +0530

    Update Importing User Forms.py

--- Importing User Forms.py
+++ Importing User Forms.py
@@ -6,3 +6,8 @@ for j,y in enumerate(myPicList):
     img = cv2.imread(path+"/"+y)
 kp2, des2 = orb.detectAndCompute(img, None)
 imgKp2 = cv2.drawKeypoints(img,kp2,None)
+bf = cv2.BFMatcher(cv2.NORM_HAMMING)
+matches = bf.match(des2, des1)
+matches.sort(key = lambda x: x.distance)
+good = matches[:int(len(matches)*(per/100))]
+imgMatches = cv2.drawMatches(img, kp2, imgQ, kp1, good,None, flags=2)
